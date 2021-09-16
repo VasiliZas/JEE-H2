18a20e980ff714ec470f090776a1e2bc11987913
VasiliZas <vasili.zasinets@gmail.com>
2021-09-17 02:03:40 +0300

    first commit in branch dev

---
+++ project/src/main/java/jee/Application.java
@@ -0,0 +1,12 @@
+package jee;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+    private static Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello world!");
+    }
+}
