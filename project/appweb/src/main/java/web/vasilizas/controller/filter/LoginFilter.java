da676a27d7731612e787526e50b32bcacb23c3ca
VasiliZas <vasili.zasinets@gmail.com>
2021-10-09 18:09:49 +0300

    LoginFilter work!!!!!!!

--- project/appweb/src/main/java/web/vasilizas/controller/filter/LoginFilter.java
+++ project/appweb/src/main/java/web/vasilizas/controller/filter/LoginFilter.java
@@ -1,42 +1,48 @@
-//package web.vasilizas.controller.filter;
-//
-//import javax.servlet.*;
-//import javax.servlet.annotation.WebFilter;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import javax.servlet.http.HttpSession;
-//import java.io.IOException;
-//
-//@WebFilter(filterName = "LoginFilter", urlPatterns = "/*")//Все страницы сайта обрабатывает данный фильтр
-//public class LoginFilter extends AbstractFilter {
-//
-//
-//    @Override
-//    public void init(FilterConfig filterConfig) throws ServletException {
-//
-//    }
-//
-//    @Override
-//    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-//
-//        HttpServletRequest request = (HttpServletRequest) req;
-//        HttpServletResponse response = (HttpServletResponse) res;
-//        HttpSession session = request.getSession();
-//
-//        String loginURI = request.getContextPath() + "/auth.html";
-//        String type = (String) session.getAttribute("type");
-//
-//        boolean loggedIn = type == null || !type.equals("Admin");
-//        //boolean loginRequest = !request.getRequestURI().equals(loginURI);
-//
-//        if (loggedIn ) {
-//            response.sendRedirect("/myweb/auth.html");
-//return;
-//        }
-//        chain.doFilter(request, response);
-//    }
-//
-//    @Override
-//    public void destroy() {
-//    }
-//}
+package web.vasilizas.controller.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(filterName = "LoginFilter", urlPatterns = "/*")//Все страницы сайта обрабатывает данный фильтр
+public class LoginFilter extends AbstractFilter {
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+//init
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        HttpSession session = request.getSession();
+
+        String loginURI = "/myweb/auth";
+        String type = (String) session.getAttribute("type");
+
+        if (type == null) {
+            type = "null";
+        }
+
+        boolean loggedIn = type.equals("Admin") || type.equals("Student") || type.equals("Teacher");
+        boolean loginRequest = request.getRequestURI().equals(loginURI);
+
+        if (loggedIn || loginRequest) {
+            chain.doFilter(request, response);
+
+        } else {
+            response.sendRedirect("/myweb/auth.html");
+        }
+    }
+
+    @Override
+    public void destroy() {
+        //destroy
+    }
+}
