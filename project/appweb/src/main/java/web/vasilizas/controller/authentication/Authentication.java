9ede802649fc5eda0938bdd77755bfd001ecd67a
VasiliZas <vasili.zasinets@gmail.com>
2021-10-09 09:54:29 +0300

    fixed some problem with redirect

--- project/appweb/src/main/java/web/vasilizas/controller/authentication/Authentication.java
+++ project/appweb/src/main/java/web/vasilizas/controller/authentication/Authentication.java
@@ -4,7 +4,6 @@ import vasilizas.myservice.security.AdminSecurity;
 import vasilizas.myservice.security.StudentSecurity;
 import vasilizas.myservice.security.TeacherSecurity;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,12 +15,12 @@ import java.io.IOException;
 public class Authentication extends HttpServlet {
 
     @Override
-    public void init() throws ServletException {
+    public void init() {
         AdminSecurity.security("Vasili", "mylogin", "456987");
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String name = request.getParameter("name");
         String type = request.getParameter("type");
         String login = request.getParameter("login");
@@ -34,32 +33,31 @@ public class Authentication extends HttpServlet {
             session.setAttribute("login", login);
             session.setAttribute("name", name);
             response.sendRedirect("/myweb/student/student.html");
+            return;
         }
-
         if (type.equals("Admin") && AdminSecurity.check(name, login, password)) {
             session.setAttribute("type", type);
             session.setAttribute("login", login);
             session.setAttribute("name", name);
             response.sendRedirect("/myweb/admin/admin.html");
+            return;
         }
-
         if (type.equals("Teacher") && TeacherSecurity.check(name, login, password)) {
             session.setAttribute("login", login);
             session.setAttribute("type", type);
             session.setAttribute("name", name);
             response.sendRedirect("/myweb/teacher/teacher.html");
-        }
-
-        else {
-            session.setAttribute("login", null);
-            session.setAttribute("type", null);
-            session.setAttribute("name", null);
-            response.sendRedirect("/myweb/error/auth-error.html");
+            return;
+        } else  {
+            session.setAttribute("login", "null");
+            session.setAttribute("type", "null");
+            session.setAttribute("name", "null");
+             response.sendRedirect("/myweb/error/auth-error.html");
+             return;
         }
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-
     }
 }
