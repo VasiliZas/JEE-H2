445efc5fc4b3fe64766f8793294129402a3d9b7e
VasiliZas <vasili.zasinets@gmail.com>
2021-11-01 19:46:21 +0300

    hibernate persist

--- project/appweb/src/main/java/web/vasilizas/Start.java
+++ project/appweb/src/main/java/web/vasilizas/Start.java
@@ -2,12 +2,14 @@ package web.vasilizas;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import vasilizas.bean.db.Marks;
 import vasilizas.bean.db.StudentDb;
 import vasilizas.exception.MyWebAppException;
 import web.vasilizas.repositories.EntityManagerHelper;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceException;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
@@ -18,14 +20,27 @@ public class Start {
             EntityManager em = EntityManagerHelper.getInstance().getEntityManager();
             EntityTransaction tx = em.getTransaction();
             tx.begin();
-            StudentDb studentDb = em.find(StudentDb.class, 10003);
-            em.remove(studentDb);
+//            var student = new StudentDb().withAge(562)
+//                    .withLogin("Gaws")
+//                    .withName("Nam2")
+//                    .withPassword("qwerty")
+//                    .setStudentId();
+
+            var grade = new Marks().withGrade(88).withTheme("Eating").withId(10000);
+
+            //StudentDb studentDb = em.find(StudentDb.class, 10003);
+            em.persist(grade);
+
+            // em.remove(studentDb);
             TypedQuery<StudentDb> fromStudentDb = em.createQuery("from StudentDb ", StudentDb.class);
+            TypedQuery<Marks> fromGrade = em.createQuery("from Marks ", Marks.class);
             List<StudentDb> dbList = fromStudentDb.getResultList();
+            List<Marks> dbGrade = fromGrade.getResultList();
+            dbGrade.forEach(g -> log.info(" !!!!!!  {}", g));
             dbList.forEach(e -> log.info(" !!!!!!  {}", e));
             tx.commit();
             em.close();
-        } catch (MyWebAppException exception) {
+        } catch (MyWebAppException | PersistenceException exception) {
             log.error("Exception : ", exception);
         }
     }
--- project/model/src/main/java/vasilizas/bean/db/Marks.java
+++ project/model/src/main/java/vasilizas/bean/db/Marks.java
@@ -2,20 +2,37 @@ package vasilizas.bean.db;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-import lombok.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @ToString(callSuper = true)
 @EqualsAndHashCode()
 @Data
-@AllArgsConstructor
+
 @NoArgsConstructor
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 
 public class Marks {
 
+
     private Integer id;
-
     private String theme;
-
     private Integer grade;
+
+    public Marks withGrade(Integer grade) {
+        setGrade(grade);
+        return this;
+    }
+
+    public Marks withTheme(String theme) {
+        setTheme(theme);
+        return this;
+    }
+
+    public Marks withId(Integer id) {
+        setId(id);
+        return this;
+    }
 }
