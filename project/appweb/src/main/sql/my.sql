c89acc22bc60a32197ace5b3f2c8b85234a8d694
VasiliZas <vasili.zasinets@gmail.com>
2021-11-14 17:46:18 +0300

    sql file

--- project/appweb/src/main/sql/my.sql
+++ project/appweb/src/main/sql/my.sql
@@ -85,3 +85,66 @@ CREATE UNIQUE INDEX themes_id_uindex ON my.themes USING btree (id);
 
 ALTER TABLE my.themes
     ADD CONSTRAINT themes_group_id_fk FOREIGN KEY (idgroup) REFERENCES my."group" (id);
+
+INSERT INTO my.grade (id, theme, grade, stuid, "groups")
+VALUES (1, 'Run', 88, 10001, 'Second');
+INSERT INTO my.grade (id, theme, grade, stuid, "groups")
+VALUES (2, 'Sleep', 54, 10001, 'Second');
+INSERT INTO my.grade (id, theme, grade, stuid, "groups")
+VALUES (3, 'Run', 69, 10002, 'Second');
+INSERT INTO my.grade (id, theme, grade, stuid, "groups")
+VALUES (4, 'Sleep', 79, 10002, 'Second');
+INSERT INTO my.grade (id, theme, grade, stuid, "groups")
+VALUES (5, 'History', 99, 10002, 'First');
+INSERT INTO my.grade (id, theme, grade, stuid, "groups")
+VALUES (6, 'History', 42, 10001, 'First');
+INSERT INTO my.grade (id, theme, grade, stuid, "groups")
+VALUES (7, 'Run', 87, 10001, 'First');
+INSERT INTO my.grade (id, theme, grade, stuid, "groups")
+VALUES (8, 'Run', 32, 10002, 'First');
+
+INSERT INTO my.teacher (id, "name", login, "password", age)
+VALUES (20001, 'Fox', 'fox', '123', 46);
+INSERT INTO my.teacher (id, "name", login, "password", age)
+VALUES (20002, 'Picasso', 'picasso', '456', 56);
+
+INSERT INTO my."group" (id, "name")
+VALUES (20002, 'First');
+INSERT INTO my."group" (id, "name")
+VALUES (20001, 'Second');
+
+INSERT INTO my.themes (id, themegroup, idgroup)
+VALUES (1, 'Sleep', 20002);
+INSERT INTO my.themes (id, themegroup, idgroup)
+VALUES (2, 'History', 20001);
+INSERT INTO my.themes (id, themegroup, idgroup)
+VALUES (3, 'Run', 20001);
+INSERT INTO my.themes (id, themegroup, idgroup)
+VALUES (4, 'Run', 20002);
+
+INSERT INTO my.salary (id, salary, sid)
+VALUES (1, 2000, 20001);
+INSERT INTO my.salary (id, salary, sid)
+VALUES (2, 2555, 20001);
+INSERT INTO my.salary (id, salary, sid)
+VALUES (3, 2480, 20001);
+INSERT INTO my.salary (id, salary, sid)
+VALUES (4, 1980, 20002);
+INSERT INTO my.salary (id, salary, sid)
+VALUES (5, 2102, 20002);
+INSERT INTO my.salary (id, salary, sid)
+VALUES (6, 2460, 20002);
+
+INSERT INTO my.student (id, "name", login, "password", age)
+VALUES (10001, 'Bakke', 'bakke', '456', 25);
+INSERT INTO my.student (id, "name", login, "password", age)
+VALUES (10002, 'Jasmin', 'jasmin', '123', 22);
+
+INSERT INTO my.studgr (student_id, group_id)
+VALUES (10001, 20001);
+INSERT INTO my.studgr (student_id, group_id)
+VALUES (10001, 20002);
+INSERT INTO my.studgr (student_id, group_id)
+VALUES (10002, 20001);
+INSERT INTO my.studgr (student_id, group_id)
+VALUES (10002, 20002);
